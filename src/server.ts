b25d73b8d3e431fe8a83d7fef510009c7f84b36a
Jefferson da Silva <je_jss@hotmail.com>
2021-04-06 02:09:38 -0300

    Include on project swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specification.routes";
+import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
+app.use(router);
 app.listen(3333, () => console.log("Server is Running!"));
