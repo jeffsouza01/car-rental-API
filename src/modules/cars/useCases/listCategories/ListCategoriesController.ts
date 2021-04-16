b77d2003e8c46e5c175e10d8f68e9bdcc3e1e662
Jefferson da Silva <je_jss@hotmail.com>
2021-04-16 03:29:42 -0300

    Update tsyringe and database usability

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
-
   async handle(request: Request, response: Response): Promise<Response> {
-    const list = await this.listCategoriesUseCase.execute();
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
+
+    const list = await listCategoriesUseCase.execute();
 
     return response.json(list);
   }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import "./database";
+import "./shared/container";
 import express from "express";
 import swaggerUI from "swagger-ui-express";
 
