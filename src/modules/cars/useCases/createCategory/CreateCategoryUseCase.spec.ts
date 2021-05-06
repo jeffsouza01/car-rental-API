e33de92547f7846ca3e10cef831461b430359866
Jefferson da Silva <je_jss@hotmail.com>
2021-05-06 01:46:39 -0300

    Initialize Tests on CreateCategory

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,8 @@
+describe("Create Category", () => {
+  it("should to be able create a category", () => {
+    const soma = 2 + 2;
+    const result = 4;
+
+    expect(soma).toBe(result);
+  });
+});
